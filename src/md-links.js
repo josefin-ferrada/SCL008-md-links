532f925ed7b1c02a41fc44fc7f5733d6c1f51e8f
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-04-22 00:23:59 -0400

    leyendo href

--- src/md-links.js
+++ src/md-links.js
@@ -1,26 +1,79 @@
 const fs = require('fs');
 
-fs.readFile('./README.md', 'utf-8',(err, data) => {
-  if (err) throw err;
-  ;
-});
-
-
 var markdownLinkExtractor = require('markdown-link-extractor');
 
-var markdown = fs.readFileSync('README.md').toString();
+
+var markdown = fs.readFileSync('./../README.md').toString();
 
 var links = markdownLinkExtractor(markdown);
 
+
+
 links.forEach(function (link) {
     console.log(link);
-
+   
+	
+	 
+/*	urlStatusCode(link, (error, statusCode) => {
+	  if (error) {
+	    console.error(error)
+	  } else {
+	    console.log(statusCode)
+	  }
+	})
+*/
 
 });
 
-// esta de abajo es solo una prueba para leer el status de los links pero no funciona aun
 
-fs.readlink("./README.md", function (err, linkString) { 
-         // .. do some error handling here .. 
-         console.log(linkString) 
+
+
+
+
+
+
+
+
+
+/*const fs = require("fs");
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+fs.readFile("./../README.md", "utf-8", (err, data) => {
+  if (err) throw err;
 });
+
+var markdownLinkExtractor = require("markdown-link-extractor");
+
+var markdown = fs.readFileSync("./../README.md").toString();
+
+var links = markdownLinkExtractor(markdown);
+
+let linksAndStatuses = [];
+links.forEach(function(link) {
+  let object = {};
+  object.link = link;
+  object.status = getStatus(link);
+  linksAndStatuses.push(object);
+});
+console.log(linksAndStatuses);*/
+/*fs.readlink("README.md", function(err, linkString) {
+  // .. do some error handling here ..
+  console.log(linkString);
+});*/
+/*function getStatus(url) {
+  let state = null;
+  var request = new XMLHttpRequest();
+
+  request.open("GET", url, true);
+  request.send();
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      console.log("status code: " + request.status); //this contains the status code
+      state = request.status;
+    } else {
+      request.abort;
+    }
+    return state;
+  };
+}
+*/
